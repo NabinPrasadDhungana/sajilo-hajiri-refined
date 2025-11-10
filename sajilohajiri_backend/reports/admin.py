6a5237a78c0244b6509c48b8ed5547be7f7f36fb
Nabin Prasad Dhungana <prnabin123@gmail.com>
2025-11-10 15:56:03 +0545

    models created and registered to admin for reports app

--- sajilohajiri_backend/reports/admin.py
+++ sajilohajiri_backend/reports/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import AttendanceReport
 
 # Register your models here.
+admin.site.register(AttendanceReport)
--- sajilohajiri_backend/reports/models.py
+++ sajilohajiri_backend/reports/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.conf import settings
+from academics.models import ClassSubject
 
 # Create your models here.
+class AttendanceReport(models.Model):
+    REPORT_TYPE_CHOICES = [
+        ('individual', 'Individual'),
+        ('class', 'Class'),
+        ('subject', 'Subject'),
+    ]
+    STATUS_CHOICES = [
+        ('generated', 'Generated'),
+        ('processing', 'Processing'),
+        ('downloaded', 'Downloaded'),
+        ('failed', 'Failed'),
+    ]
+
+    generated_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    student = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.SET_NULL, related_name='report_student')
+    class_subject = models.ForeignKey(ClassSubject, null=True, blank=True, on_delete=models.SET_NULL)
+    from_date = models.DateField()
+    to_date = models.DateField()
+    report_type = models.CharField(max_length=20, choices=REPORT_TYPE_CHOICES)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='generated')
+    report_file = models.FileField(upload_to='attendance_reports/', null=True, blank=True)
+    title = models.CharField(max_length=255, null=True, blank=True)
+    generated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title or f"{self.report_type} report from {self.from_date} to {self.to_date}"
