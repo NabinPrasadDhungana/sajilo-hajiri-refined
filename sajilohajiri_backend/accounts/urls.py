428b8a61bf0d90f93a6c40c332b287cd4915fe5a
Nabin Prasad Dhungana <prnabin123@gmail.com>
2025-11-10 17:46:40 +0545

    media configuration and viewset router configuration done

---
+++ sajilohajiri_backend/accounts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet
+
+router = DefaultRouter()
+
+router.register('users', UserViewSet, basename='user')
+
+urlpatterns = [
+    
+]
+
+urlpatterns += router.urls
