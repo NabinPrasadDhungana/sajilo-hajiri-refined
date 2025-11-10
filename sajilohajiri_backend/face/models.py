b28f439bb2a2e063bb878ec77f2a6723cc558dbc
Nabin Prasad Dhungana <prnabin123@gmail.com>
2025-11-10 15:45:45 +0545

    models created and registered in admin for face app

--- sajilohajiri_backend/face/models.py
+++ sajilohajiri_backend/face/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class FaceEncoding(models.Model):
+    student = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, limit_choices_to={'role': 'student'})
+    encoding_data = models.TextField()  # Store as base64 or JSON string
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Face data for {self.student.username}"
