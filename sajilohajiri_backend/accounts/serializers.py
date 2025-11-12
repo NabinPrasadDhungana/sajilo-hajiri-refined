315f9ba4a2eaf8a7f99c34cbb5235454c380161d
Nabin Prasad Dhungana <prnabin123@gmail.com>
2025-11-12 14:17:48 +0545

    Password validated in serializer

--- sajilohajiri_backend/accounts/serializers.py
+++ sajilohajiri_backend/accounts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import User
+from django.core.validators import RegexValidator
 
 # serializers go down here
 class UserSerializer(serializers.ModelSerializer):
@@ -7,8 +8,12 @@ class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(
         write_only=True,
         required=True,
-        style={'input_type': 'password', 'placeholder': 'Password'}
-        )
+        style={'input_type': 'password', 'placeholder': 'Password'},
+        validators=[RegexValidator(
+            regex=r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*-])[A-Za-z\d!@#$%^&*-]{8,32}$',
+            message='The password must be 8-32 characters, include at least one lowercase, one uppercase, one digit, and one special character from !@#$%^&*-, and contain only those allowed characters.'
+        )]
+    )
 
     class Meta:
         model = User
