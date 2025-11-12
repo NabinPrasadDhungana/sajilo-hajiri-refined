3e0db91fdd1746f764de14012cf10e5fd465adf4
Nabin Prasad Dhungana <prnabin123@gmail.com>
2025-11-12 13:41:49 +0545

    username field removed and replaced with email

--- sajilohajiri_backend/accounts/models.py
+++ sajilohajiri_backend/accounts/models.py
@@ -1,6 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.urls import reverse
+from django.contrib.auth.base_user import BaseUserManager
+
+# Custom user manager
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(email, password, **extra_fields)
 
 # Create your models here.
 class User(AbstractUser):
@@ -26,6 +49,8 @@ class User(AbstractUser):
         ('approved', 'Approved'),
         ('unapproved', 'Unapproved'),
     ]
+    username = None
+    email = models.EmailField(unique=True)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
     name = models.CharField(max_length=150, blank=True, null=True)
@@ -36,5 +61,8 @@ class User(AbstractUser):
     approval_status = models.CharField(max_length=15, choices=APPROVAL_CHOICES, default='pending')
     feedback = models.TextField(blank=True, null=True)  # feedback if unapproved
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['role', 'avatar', 'name']
+
     def __str__(self):
-        return self.name or self.username
+        return self.name or self.email
