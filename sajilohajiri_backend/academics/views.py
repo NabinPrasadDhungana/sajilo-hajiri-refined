a3e0f4a2dad2a37da597d3553edc7b297e22c45f
Nabin Prasad Dhungana <prnabin123@gmail.com>
2025-11-13 18:47:01 +0545

    filter for academic app's models created

--- sajilohajiri_backend/academics/views.py
+++ sajilohajiri_backend/academics/views.py
@@ -4,24 +4,29 @@ from rest_framework import viewsets
 from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
 from .serializers import ClassSerializer, SubjectSerializer, ClassSubjectSerializer, StudentClassEnrollmentSerializer
 from .permissions import AdminRole
+from .filters import ClassFilter, SubjectFilter, ClassSubjectFilter, StudentClassEnrollmentFilter
 
 # Create your views here.
 class ClassViewSet(viewsets.ModelViewSet):
     queryset = Class.objects.all()
     serializer_class = ClassSerializer
     permission_classes = [AdminRole]
+    filterset_class = ClassFilter
 
 class SubjectViewSet(viewsets.ModelViewSet):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
     permission_classes = [AdminRole]
+    filterset_class = SubjectFilter
 
 class ClassSubjectViewSet(viewsets.ModelViewSet):
     queryset = ClassSubject.objects.all()
     serializer_class = ClassSubjectSerializer
     permission_classes = [AdminRole]
+    filterset_class = ClassSubjectFilter
 
 class StudentClassEnrollmentViewSet(viewsets.ModelViewSet):
     queryset = StudentClassEnrollment.objects.all()
     serializer_class = StudentClassEnrollmentSerializer
     permission_classes = [AdminRole]
+    filterset_class = StudentClassEnrollmentFilter
