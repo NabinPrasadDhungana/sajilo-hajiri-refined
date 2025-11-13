93494fdb4f9b03388552f0bb2db2bb78aecc4302
Nabin Prasad Dhungana <prnabin123@gmail.com>
2025-11-13 16:21:41 +0545

    superuser added to the has permission

--- sajilohajiri_backend/academics/permissions.py
+++ sajilohajiri_backend/academics/permissions.py
@@ -3,4 +3,4 @@ from rest_framework.permissions import BasePermission
 class AdminRole(BasePermission):
 
     def has_permission(self, request, view):
-        return request.user.role=='admin'
+        return request.user.role=='admin' or request.user.is_superuser
