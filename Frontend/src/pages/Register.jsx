9e59730344584acea40360c9b2ecf1a8b086b63f
SURIYA-po <surya.221521@ncit.edu.np>
2025-11-14 19:22:09 +0545

    Web_cam_capture_edited

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import WebcamCapture from '../services/webcamcapture';
+import WebcamCapture from '../services/WebcamCapture';
 
 import { Navigate, useNavigate } from 'react-router-dom';
 import { useContext } from "react";
