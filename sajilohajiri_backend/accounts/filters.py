5c9ba40c181abbe915c4f6d87f1d77c80ba3930f
Nabin Prasad Dhungana <prnabin123@gmail.com>
2025-11-14 18:22:58 +0545

    unnecessary Filtering fields filtered out from UserFilter

--- sajilohajiri_backend/accounts/filters.py
+++ sajilohajiri_backend/accounts/filters.py
@@ -6,10 +6,7 @@ class UserFilter(django_filters.FilterSet):
     class Meta:
         model = User
         fields = {
-            'email': ['icontains'],
-            'name': ['icontains'],
             'role': ['exact'],
-            'roll_number': ['contains'],
             'semester': ['exact'],
             'section': ['exact'],
             'department': ['exact'],
