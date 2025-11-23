a05baee6b9281e264ca5c79fbb8c6b337718f455
Nabin Prasad Dhungana <prnabin123@gmail.com>
2025-11-23 18:59:47 +0545

    admin Role custom permission allowed for get requests

--- sajilohajiri_backend/academics/permissions.py
+++ sajilohajiri_backend/academics/permissions.py
@@ -3,4 +3,6 @@ from rest_framework.permissions import BasePermission
 class AdminRole(BasePermission):
 
     def has_permission(self, request, view):
+        if request.method == 'GET':
+            return True
         return request.user.role=='admin' or request.user.is_superuser
