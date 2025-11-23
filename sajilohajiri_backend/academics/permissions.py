dcbd889bd1adbaf42c0f29827d2cd6360b90f366
Nabin Prasad Dhungana <prnabin123@gmail.com>
2025-11-23 20:05:31 +0545

    custom permissions written for allowing olny authenticated for get, and rest of the request method requiring admin permission

--- sajilohajiri_backend/academics/permissions.py
+++ sajilohajiri_backend/academics/permissions.py
@@ -3,6 +3,14 @@ from rest_framework.permissions import BasePermission
 class AdminRole(BasePermission):
 
     def has_permission(self, request, view):
-        if request.method == 'GET':
+        return request.user.role=='admin' or request.user.is_staff
+    
+class OnlyAuthenticated(BasePermission):
+
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+        else:
+            if request.method in ['POST', 'PATCH', 'DELETE', 'PUT'] and not (request.user.role=='admin' or request.user.is_staff): 
+                return False
             return True
-        return request.user.role=='admin' or request.user.is_superuser
--- sajilohajiri_backend/academics/views.py
+++ sajilohajiri_backend/academics/views.py
@@ -3,30 +3,30 @@ from .models import Class, Subject, ClassSubject, StudentClassEnrollment
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
 from .serializers import ClassSerializer, SubjectSerializer, ClassSubjectSerializer, StudentClassEnrollmentSerializer
-from .permissions import AdminRole
+from .permissions import OnlyAuthenticated
 from .filters import ClassFilter, SubjectFilter, ClassSubjectFilter, StudentClassEnrollmentFilter
 
 # Create your views here.
 class ClassViewSet(viewsets.ModelViewSet):
     queryset = Class.objects.all()
     serializer_class = ClassSerializer
-    permission_classes = [AdminRole]
+    permission_classes = [OnlyAuthenticated]
     filterset_class = ClassFilter
 
 class SubjectViewSet(viewsets.ModelViewSet):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
-    permission_classes = [AdminRole]
+    permission_classes = [OnlyAuthenticated]
     filterset_class = SubjectFilter
 
 class ClassSubjectViewSet(viewsets.ModelViewSet):
     queryset = ClassSubject.objects.all()
     serializer_class = ClassSubjectSerializer
-    permission_classes = [AdminRole]
+    permission_classes = [OnlyAuthenticated]
     filterset_class = ClassSubjectFilter
 
 class StudentClassEnrollmentViewSet(viewsets.ModelViewSet):
     queryset = StudentClassEnrollment.objects.all()
     serializer_class = StudentClassEnrollmentSerializer
-    permission_classes = [AdminRole]
+    permission_classes = [OnlyAuthenticated]
     filterset_class = StudentClassEnrollmentFilter
